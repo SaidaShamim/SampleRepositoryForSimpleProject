c3158e493f70d570cf06c7e34e8cf2caef45e2be
BH04871 <Jyothi.Panyala@macys.com>
2018-03-20 15:39:31 +0530

    add jyothi

--- FirstSimpleProject/src/com/jyothi/A.java
+++ FirstSimpleProject/src/com/jyothi/A.java
@@ -2,6 +2,7 @@ package com.jyothi;
 
 public class A {
 public static void main(String[] args) {
-	System.out.println("first print statement");
+	String a="JYOTHI";
+	System.out.println("first print statement" + a);
 }
 }
